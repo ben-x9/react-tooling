b341df268976a6f838c06d6d4e4f5861a40d981a
Ben Lambert <ben@joyxer.com>
2018-01-11 12:25:53 +0900

    Remove dist/ from repo

--- dist/dispatchMiddleware.d.ts
+++
@@ -1,5 +0,0 @@
-import { MiddlewareAPI, Dispatch, Action } from "redux";
-declare const dispatchMiddleware: <S, A extends Action>(store: MiddlewareAPI<S>) => (next: Dispatch<S>) => (action: A) => A & {
-    dispatch: (action: A) => void;
-};
-export default dispatchMiddleware;
