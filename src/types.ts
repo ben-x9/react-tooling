c0379e70d9ebb4620fa9821599c6cd9ea58f2bf3
Ben Lambert <ben@joyxer.com>
2018-04-05 17:25:20 +0900

    Add deep types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,10 @@
+import List from "./list"
 export type Nothing = null | undefined | void
 export type Maybe<T> = T | Nothing
 export type Type<T> = (x: T) => T
+
+export interface DeepList<T> extends List<T | DeepList<T>> {}
+export type Deep<T> = T | DeepList<T>
+
+export interface DeepArray<T> extends List<T | DeepArray<T>> {}
+export type DeepA<T> = T | DeepArray<T>
