0ae6f8f866426029bd96e44a1544bdd66f2b3d16
Chaabane Jalal <jalal.chaabane@epitech.eu>
2018-06-08 13:48:57 +0900

    simplify reactsTo function by using isActionOf instead of switch (#5)

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ import List from "./list"
 export type Nothing = null | undefined | void
 export type Maybe<T> = T | Nothing
 export type Type<T> = (x: T) => T
+export type F1<A, B> = (x: A) => B
 
 export interface DeepList<T> extends List<T | DeepList<T>> {}
 export type Deep<T> = T | DeepList<T>
