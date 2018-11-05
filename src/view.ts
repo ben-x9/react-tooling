db09f732b72bad476a76f8aeefa04a3d3844b29b
jalald <jalal@nulab-inc.com>
2018-11-05 11:27:00 +0900

    fix memoize and add debug option to log when diff are detected

--- src/view.ts
+++ src/view.ts
@@ -7,21 +7,24 @@ export type View<State> =
   | ((state: State) => JSXElement)
   | {new (state: State & Dispatcher<State>): React.Component<State>}
 
-const shallowEqual = (first: any, second: any): boolean =>
+const shallowEqual = (first: any, second: any, logDiff?: boolean): boolean =>
   Object.keys(first).every(key => {
-    if (first[key] && isDispatchUpdate(first[key])) return true
+    if (first[key] && isDispatchUpdate(first[key]) || typeof first[key] === "function")
+      return true
+    if (logDiff && first[key] !== second[key])
+      console.log("diff: ", first[key], second[key])
     return first[key] === second[key]
   })
 
-export const memoizeComponent = <S>(view: View<S>): View<S> => {
+export const memoizeComponent = <S>(view: View<S>, logDiff?: boolean): View<S> => {
   const factory = React.createFactory(view as any)
-  return class MemoizedComponent extends React.Component<S> {
-    shouldComponentUpdate(nextProps: S) {
-      return !shallowEqual(this.props, nextProps)
-    }
+    return class MemoizedComponent extends React.Component<S> {
+      shouldComponentUpdate(nextProps: S) {
+        return !shallowEqual(this.props, nextProps, logDiff)
+      }
 
-    render() {
-      return factory(this.props as any)
+      render() {
+        return factory(this.props as any)
+      }
     }
-  }
 }
