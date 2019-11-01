7e483a9f9e3be9ee5b6ef43be50391f818333f61
Chaabane Jalal <jalal.chaabane@epitech.eu>
2019-11-01 05:12:26 +0100

    allow comparator to be passed to memoized component (#30)
    * allow comparator to be passed to memoized component
    
    * commit package-lock.json

--- src/view.ts
+++ src/view.ts
@@ -1,5 +1,7 @@
 import {isDispatch} from "hydra-dispatch"
 import React from "react"
+import { F2 } from "functools-ts"
+// import {F2} from "functools-ts"
 
 export type JSXElement = React.ReactElement<any>
 
@@ -7,7 +9,7 @@ export type View<State> =
   | ((state: State) => JSXElement)
   | {new (state: State): React.Component<State>}
 
-const shallowEqual = (first: any, second: any, logDiff?: boolean): boolean =>
+export const shallowEqual = (first: any, second: any, logDiff?: boolean): boolean =>
   Object.keys(first).every(key => {
     if (
       (first[key] && isDispatch(first[key])) ||
@@ -15,17 +17,20 @@ const shallowEqual = (first: any, second: any, logDiff?: boolean): boolean =>
     )
       return true
     if (logDiff && first[key] !== second[key])
-      console.log("diff: ", first[key], second[key])
+      console.log(`diff: ${key}`, first[key], second[key])
     return first[key] === second[key]
   })
 
 export const memoizeComponent = <S>(
   view: View<S>,
-  logDiff?: boolean
+  logDiff?: boolean,
+  compare?: F2<S, S, boolean>
 ): View<S> => {
   const factory = React.createFactory(view as any)
   return class MemoizedComponent extends React.Component<S> {
     shouldComponentUpdate(nextProps: S) {
+      if (compare)
+        return !compare(this.props, nextProps)
       return !shallowEqual(this.props, nextProps, logDiff)
     }
 
